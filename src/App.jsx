8dd3ed4b7343074d8bdd91479635ec8ee85ec248
kaaylevirmar <kylevirmarmillendez@gmail.com>
2024-03-04 21:51:30 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My Portfolio</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        molestiae asperiores, quis porro autem facere ratione illum est atque
+        eos obcaecati natus quos excepturi magni! Iusto deleniti doloribus neque
+        nesciunt.
+      </p>
+    </>
+  );
+}
+
+export default App;
